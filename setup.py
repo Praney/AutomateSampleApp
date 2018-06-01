6e6a158e593c5d4e345a6a9f6edbf6078e3bc0db
praneymadan171@gmail.com <praneymadan171@gmail.com>
2018-06-01 12:19:56 +0530

    corrected typos and introduced setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+from os import path
+setup(name = 'shpock-test',
+version = '1.0',
+description = 'Test Strategry for Shpock',
+author = 'Praney Madan',
+packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+)
